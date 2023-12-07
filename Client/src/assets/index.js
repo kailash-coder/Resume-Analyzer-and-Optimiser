23b4a46d83b004bea0a1cee10aef754c53135573
Sarvani <129776752+itssarvani394@users.noreply.github.com>
2023-12-07 21:26:18 +0530

    Delete Client directory

--- Client/src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-import image from './image.png';
-import backimg from './backimg.png'
-
-export {
-    logo,
-    image,
-    backimg,
-};
--- Client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home';
-import Analysis from './Analysis'
-
-export {
-    Home,
-    Analysis
-}
